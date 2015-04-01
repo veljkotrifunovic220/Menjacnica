68954f524465668180edde88084fddbd6b72fc86
veljkotrifunovic220 <veljko.trifun@gmail.com>
2015-04-01 19:29:23 +0200

    Napravljena klasa Menjacnica koja implementira sve metode iz interfejsa MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,21 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajValutu(Valuta valuta) {
+
+	}
+
+	public void obrisiValutu(Valuta valuta) {
+
+	}
+
+	public Valuta pronadjiKurs(String naziv, GregorianCalendar datum) {
+		return null;
+	}
+
+}

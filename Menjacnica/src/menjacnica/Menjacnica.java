fd2848d01a14e16d7c98ac32d1d98b5e0e51b1b5
veljkotrifunovic220 <veljko.trifun@gmail.com>
2015-04-02 01:30:23 +0200

    U klasi Menjacnica implementirana metoda dodajValutu - master grana

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,13 +1,20 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodajValutu(Valuta valuta) {
-
+		if ( (valuta == null) || (valute.contains(valuta)) ) {
+			throw new RuntimeException("Greska! Valuta ne sme biti null ili je valuta vec uneta!");
+		}
+		
+		valute.add(valuta);
 	}
 
 	public void obrisiValutu(Valuta valuta) {

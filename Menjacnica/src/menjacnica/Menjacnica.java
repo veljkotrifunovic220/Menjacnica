be735c4c278ba5e3a1186b65a75c55e2a8be69cd
veljkotrifunovic220 <veljko.trifun@gmail.com>
2015-04-02 01:17:53 +0200

    U klasi Menjacnica implementirane sve metode iz interfejsa MenjacnicaInterfejs - grana implementacijainterfejsa

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,21 +1,54 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodajValutu(Valuta valuta) {
-
+		if (valuta == null) {
+			throw new RuntimeException("Greska! Valuta ne sme biti null!");
+		}
+		
+		if (valute.contains(valuta)) {
+			throw new RuntimeException("Greska! Ova valuta je vec uneta!");
+		}
+		
+		valute.add(valuta);
 	}
 
 	public void obrisiValutu(Valuta valuta) {
-
+		if (valuta == null) {
+			throw new RuntimeException("Greska! Valuta ne sme biti null!");
+		}
+		
+		if ( !(valute.contains(valuta)) ) {
+			throw new RuntimeException("Taj kurs ne postoji");
+		}
+		
+		valute.remove(valuta);
 	}
 
 	public Valuta pronadjiKurs(String naziv, GregorianCalendar datum) {
-		return null;
+		if ( (naziv == null) || (datum == null) ) {
+			throw new RuntimeException("Greska! Morate uneti i naziv valute i datum!");
+		}
+		
+		Valuta valuta = new Valuta();
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if ( (valute.get(i).getNaziv().equals(naziv) || valute.get(i).getSkraceniNaziv().equals(naziv)) 
+					&& (valute.get(i).getDatum().equals(datum)) ) {
+				valuta = valute.get(i);
+				break;
+			}
+		}
+		
+		return valuta;
 	}
 
 }
